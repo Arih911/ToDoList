40a70e4a9f07be43ec72ec268200e08044d5f6c0
arihant <kamdearihant01@gmail.com>
2022-07-17 10:38:52 +0530

    Dynamic Items Add Update

--- app.js
+++ app.js
@@ -2,13 +2,32 @@ const express = require("express");
 const bodyparser = require("body-parser");
 
 const app = express();
-// app.use("bodyparser", {extended: true})
-
+app.use(bodyparser.urlencoded({extended:true}));
+var items = ["Complete Project","Study Operating System","Code Graph"];
+app.set('view engine', 'ejs');
 app.get("/", function(req, res){
-    // res.sendFile(__dirname+"/index.html");
+   
     var today = new Date();
-    console.log(today);
-})
+
+    var options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+
+    res.render("list",{
+        kindOfDay : day, newItem : items
+    });
+});
+
+app.post("/", function(req, res){
+    var task = req.body.newItem;
+    items.push(task);
+    res.redirect("/");
+});
 
 app.listen(3000, function(){
     console.log("Server Running....");
